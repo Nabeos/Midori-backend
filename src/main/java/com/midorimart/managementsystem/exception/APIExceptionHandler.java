854d502d759bd373df5f44af6464de3da53cbfa0
Nabeos <duyanhvu8a4@gmail.com>
2022-10-11 10:42:59 +0700

    delete in database branch

--- src/main/java/com/midorimart/managementsystem/exception/APIExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.midorimart.managementsystem.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class APIExceptionHandler {
-
-}
