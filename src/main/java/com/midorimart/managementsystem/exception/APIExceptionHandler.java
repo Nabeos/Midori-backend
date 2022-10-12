f386fcf2c422749c0c64d9ac2113a259b79e61f8
Son Pham <phamhoangducson@gmail.com>
2022-10-13 00:19:27 +0700

    Custom exception

--- src/main/java/com/midorimart/managementsystem/exception/APIExceptionHandler.java
+++ src/main/java/com/midorimart/managementsystem/exception/APIExceptionHandler.java
@@ -1,8 +1,24 @@
 package com.midorimart.managementsystem.exception;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.midorimart.managementsystem.exception.custom.CustomBadRequestException;
+import com.midorimart.managementsystem.model.CustomError;
 
 @RestControllerAdvice
 public class APIExceptionHandler {
+
+    @ExceptionHandler(CustomBadRequestException.class)
+    @ResponseStatus(value=HttpStatus.BAD_REQUEST)
+    
+    public Map<String, CustomError> badRequestException(CustomBadRequestException ex){
+        return ex.getErrors();
+    }
+
 
 }
