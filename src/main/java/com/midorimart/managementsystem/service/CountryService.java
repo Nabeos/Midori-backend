06a0c43b29d8e0cb937c15cd84468a972e650bff
Nabeos <duyanhvu8a4@gmail.com>
2022-10-25 22:08:11 +0700

    save country

--- src/main/java/com/midorimart/managementsystem/service/CountryService.java
+++ src/main/java/com/midorimart/managementsystem/service/CountryService.java
@@ -1,5 +1,11 @@
 package com.midorimart.managementsystem.service;
 
+import java.util.Map;
+
+import com.midorimart.managementsystem.model.country.dto.CountryDTOResponse;
+
 public interface CountryService {
+
+    Map<String, CountryDTOResponse> getCountryByCode(String code);
 
 }
