57a75db26a583dddde3090f5dcb1e671bfecf72b
Nabeos <duyanhvu8a4@gmail.com>
2022-10-30 21:04:01 +0700

    fix cors

--- src/main/java/com/midorimart/managementsystem/security/WebSecurityConfig.java
+++ src/main/java/com/midorimart/managementsystem/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import lombok.RequiredArgsConstructor;
 
@@ -37,5 +38,10 @@ public class WebSecurityConfig {
         // add filter to validate token with any request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
+    }
+
+    @Bean
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
