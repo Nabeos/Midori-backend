7697d665661cd380f17ac62d4be5c1e0864d0818
Nabeos <duyanhvu8a4@gmail.com>
2022-10-30 17:11:23 +0700

    fix userDTO

--- src/main/java/com/midorimart/managementsystem/model/users/UserDTOLoginRequest.java
+++ src/main/java/com/midorimart/managementsystem/model/users/UserDTOLoginRequest.java
@@ -1,10 +1,14 @@
 package com.midorimart.managementsystem.model.users;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTOLoginRequest {
     private String email;
     private String password;
--- src/main/java/com/midorimart/managementsystem/model/users/UserDTOResponse.java
+++ src/main/java/com/midorimart/managementsystem/model/users/UserDTOResponse.java
@@ -2,11 +2,15 @@ package com.midorimart.managementsystem.model.users;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTOResponse {
     private int id;
     private String email;
