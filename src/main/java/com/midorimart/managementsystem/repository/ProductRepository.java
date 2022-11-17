153969d91ec69ff5dc3e627b370856ad48c28573
Nabeos <duyanhvu8a4@gmail.com>
2022-11-17 16:00:24 +0700

    fix get bestseller in home page

--- src/main/java/com/midorimart/managementsystem/repository/ProductRepository.java
+++ src/main/java/com/midorimart/managementsystem/repository/ProductRepository.java
@@ -21,7 +21,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
                         + "left join Product p on od.product_id = p.id "
                         + "left join Category c on p.category_id = c.id where category_id = :categoryId)AA)BB where Product_rank between 1 and 5 order by Category_ID";
 
-        public final String queryForBestSellerInHome = "select Product_ID from "
+        public final String queryForBestSellerInHome = "select top 20 Product_ID from "
                         + "(select *, DENSE_RANK()over(Partition by Category_ID order by total_number desc) as Product_rank from "
                         + "(select distinct c.id as Category_ID, c.name, p.id as Product_ID, sum(quantity) over(partition by product_id) as total_number "
                         + "from Order_Detail od "
